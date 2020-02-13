52d5991891f4b1ccfa476ef2b77d9a1261990988
Henrique L S Silva <lsshenrique@hotmail.com>
2020-02-13 11:31:33 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const mssql = require('mssql');
+
+mssql.Request.prototype.inputs = function(params) {
+    for (const key in params) {
+        const param = params[key]
+
+        if (typeof param === 'object' && param.type) {
+            this.input(key, param.type, param.value)
+        } else {
+            this.input(key, param)
+        }
+    }
+
+    return this
+}
+
+mssql.map.register(String, mssql.VarChar)
+
+class MSSQL {
+    constructor(sqlConfig) {
+        this._sqlConfig = sqlConfig;
+    }
+
+    get pool() {
+        return this._poolConnection;
+    }
+
+    initPool() {
+        this._poolConnection = new mssql.ConnectionPool(this._sqlConfig).connect();
+        return this._poolConnection;
+    }
+
+    async execute(sql) {
+        const pool = await this._poolConnection; // ensures that the pool has been created
+
+        try {
+            let result = null
+
+            if (typeof sql === 'string') {
+                result = await pool.request().query(sql);
+            } else {
+                let request = pool.request()
+
+                if (sql.params) {
+                    request.inputs(sql.params)
+                }
+
+                if (typeof sql.query === 'function') {
+                    result = await request.query(sql.query(request, mssql));
+                } else {
+                    result = await request.query(sql.query);
+                }
+            }
+
+            return result.recordset || [];
+        } catch (err) {
+            console.log('SQL error', err, sql.query || sql);
+            throw err
+        }
+    }
+}
+
+module.exports = MSSQL;
+module.exports.native = mssql;
